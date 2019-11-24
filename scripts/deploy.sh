6c6e0649e46f5e2240661d0f8bbe1cb047397ca3
Munish Gogna <gognamunish@gmail.com>
2019-11-24 09:06:50 +0800

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,12 @@
-git clone
+
+# Get latest code from master branch and build in current directory
+git clone https://github.com/gognamunish/student_enrollment.git .
+./mvnw clean package -DskipTests
+
+# Create Docker Image
+cd enrollment-api
+docker build -t blackone/student_enrollment .
+
+# Run Docker image
+docker run -p 8080:8080 -t blackone/student_enrollment
+
