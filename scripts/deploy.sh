3b50719b48dc72181099e674eff79173a5e175ca
Munish Gogna <gognamunish@gmail.com>
2019-11-24 09:44:21 +0800

    deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,17 @@ echo "Getting latest code from Github into folder: $PWD"
 
 # Get Code
 git clone https://github.com/gognamunish/student_enrollment.git
-cd student_enrollment/enrollment-api
-ls -lart
+cd student_enrollment
 
 # Build Code
 ./mvnw clean package -DskipTests
 
 # Create Docker Image
+cd enrollment-api
 docker build -t taoyogi/student_enrollment .
+
+# Push to Registry
+# docker push taoyogi/student_enrollment:latest
 
 # Run Docker image
 docker run -p 8080:8080 -t taoyoi/student_enrollment
